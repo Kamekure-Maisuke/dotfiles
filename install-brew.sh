1bd2ed6d0f91cdd62ac6688b315298cde868ce65
t-o-d <44114228+Kamekure-Maisuke@users.noreply.github.com>
2020-11-19 10:38:55 +0900

    Create install-brew.sh

---
+++ install-brew.sh
@@ -0,0 +1,10 @@
+if [[ $(which brew) ]]; then
+	echo "HomeBrewがありません。インストールします。"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
+echo "パッケージのインストール"
+
+brew bundle && brew cleanup
+
+echo "完了"
