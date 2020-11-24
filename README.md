6cbcd1229e210fa5f7f52891c0e10c47faac5927
KamekuraMaisuke <taisukeoono350@gmail.com>
2020-11-24 17:31:10 +0900

    install手順更新

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 - my dotfile
   - 保存用・最低限
+
+- mac環境構築
+
+```bash
+# clone
+git clone https://github.com/Kamekure-Maisuke/dotfiles.git ~/.dotfiles
+
+# setup-mac
+bash setup-mac.sh
+
+# brew setup
+bash install-brew.sh
+```
--- setup-mac.sh
+++ setup-mac.sh
@@ -4,10 +4,6 @@ GITSOURCE="https://github.com/Kamekure-Maisuke/dotfiles"
 TARGET="${HOME}/.dotfiles"
 TARGET_MAC="$TARGET/macos"
 
-mkdir -p "${TARGET}"
-
-git clone --recursive "$GITSOURCE" "$TARGET"
-
 FILELISTNAMES=$(find ${TARGET_MAC} -maxdepth 1 -type f | xargs basename)
 
 echo $FILELISTNAMES |
